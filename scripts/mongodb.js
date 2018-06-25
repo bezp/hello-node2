7f5dc84560ce2dca2d2f216b3c8340a130fbc7bf
bezalel <bezalelp@uci.edu>
2018-06-25 15:36:22 -0400

    added mongo connection

--- scripts/mongodb.js
+++ scripts/mongodb.js
@@ -4,6 +4,13 @@ const mongodb = require('mongodb');
 
 const dbClient  = mongodb.MongoClient;
 
-
+dbClient.connect('mongodb://localhost:27017', (error, db) => {
+  if (error) {
+    console.error('database dwn...');
+    return;
+  }
+  console.log('daaaaata base created');
+  db.close();
+});
 
 
